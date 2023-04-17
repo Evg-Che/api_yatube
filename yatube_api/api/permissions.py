b179a6a5f54b25b98f4dc3bb0be222f461e7380b
Евгения Черноусова <evgesha.che@yandex.ru>
2023-04-17 20:21:50 +0300

    Fix_2

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,6 +4,5 @@ from rest_framework import permissions
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """Разрешение на редактирование и удаление поста только его владельцу."""
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user)
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -10,10 +10,7 @@ from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [
-        IsAuthenticated,
-        IsOwnerOrReadOnly
-    ]
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly, )
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -26,10 +23,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [
-        IsAuthenticated,
-        IsOwnerOrReadOnly
-    ]
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly, )
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
